42edb1cfd6c9357ce6f750861139a1dc44f120cf
Chen <Amy.Chen10@student.tdsb.on.ca>
2023-03-09 14:01:29 -0500

    added javascript file

---
+++ game.js
@@ -0,0 +1,27 @@
+//String keyboard = "";import java.util.Scanner
+
+//public class game.js {
+	
+	//public static void main(String[] args) {
+		//Scanner keyboard = new Scanner(System.in)
+		//let input = ""
+        let input = prompt("Enter the correct answer:")
+        let valA = Math.random()
+        let valB = Math.random()
+        let problem = valA "+" valB
+    
+		
+		//System.out.println("Enter the correct answer:" valA "+" valB)
+       
+		//input = keyboard.nextint()
+		
+        let answer = valA + valB
+	
+		if(input == answer) {
+			System.out.println("YOU ARE CORRECT")
+		} else {
+            System.out.println("YOU ARE WRONG")
+	} 
+	
+//}		
+
